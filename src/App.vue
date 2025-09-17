b2d780423f69f73d3412b2c6f771dd9d688cda9b
skobanevandre <skobanev@yandex.ru>
2025-09-17 14:11:59 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import VueLayout from './layout.vue'
+import PrimeButton from 'primevue/button';
+</script>
+
+<template>
+  <VueLayout>
+
+  </VueLayout>
+</template>
+
+<style scoped></style>
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import PrimeVue from 'primevue/config'
+import Lara from '@primevue/themes/lara'
+import './main.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(PrimeVue, {
+  theme: { preset: Lara },
+})
+
+app.mount('#app')
